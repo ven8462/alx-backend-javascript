9f8770ec34457db9df532a4a0b6bcdd403faf72c
lavender <lavenderanyango9@gmail.com>
2024-01-03 02:07:02 +0300

    fixing linter error

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,20 +1,19 @@
 export default function iterateThroughObject(reportWithIterator) {
-    // Initialize an empty array to store the employee names
-    let employeeNames = [];
-   
-    // Use a for...of loop to iterate through the iterator
-    for (let employees of reportWithIterator) {
-     // Use another for...of loop to iterate through each employee in the current department
-     for (let employee of employees) {
-       // Add the employee name to the array
-       employeeNames.push(employee);
-     }
+  // Initialize an empty array to store the employee names
+  const employeeNames = [];
+
+  // Use a for...of loop to iterate through the iterator
+  for (const employees of reportWithIterator) {
+    // Use another for...of loop to iterate through each employee in the current department
+    for (const employee of employees) {
+      // Add the employee name to the array
+      employeeNames.push(employee);
     }
-   
-    // Join the employee names into a string, separated by ' | '
-    let employeeString = employeeNames.join(' | ');
-   
-    // Return the string
-    return employeeString;
-   }
-   
+  }
+
+  // Join the employee names into a string, separated by ' | '
+  const employeeString = employeeNames.join(' | ');
+
+  // Return the string
+  return employeeString;
+}
