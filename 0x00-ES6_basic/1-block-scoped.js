f02d73c34b858d6757789c08da1913395bdb0f3e
lavender <lavenderanyango9@gmail.com>
2024-01-02 22:59:00 +0300

    fixing block scope logic

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-    var  task = false;
-    var  task2 = true;
-  
-    if (trueOrFalse) {
-        task = true;
-        task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // no redefinition
   }
+
+  return [task, task2];
+}
