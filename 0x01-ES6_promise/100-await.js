1afe3de1da45501a2e138935b064ed7c63bd8a14
lavender <lavenderanyango9@gmail.com>
2024-01-04 01:01:32 +0300

    Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photoResponse = await uploadPhoto();
+    const userResponse = await createUser();
+
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
