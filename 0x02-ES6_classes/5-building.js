8f0a4f8fa1461b68c5dcfb87e31ce8bd4bcf660b
lavender <lavenderanyango9@gmail.com>
2024-01-09 14:34:29 +0300

    class inheritence

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage !== 'function') {
+    if (this.constructor !== Building && this.evacuationWarningMessage === 'undefined') {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
   }
