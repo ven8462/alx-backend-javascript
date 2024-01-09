c977928aef8f83b49fd3025ba487b80ce3c5745d
lavender <lavenderanyango9@gmail.com>
2024-01-09 23:51:07 +0300

    fixing a lint error

--- 0x03-ES6_data_manipulation/5-typed_array.js
+++ 0x03-ES6_data_manipulation/5-typed_array.js
@@ -1,12 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const typedArray = new Int8Array(buffer);
+  const dv = new ArrayBuffer(length);
+  const int8Array = new Int8Array(dv);
 
-  if (value < -128 || value > 127) {
-    throw new Error('Position outside range');
-  }
+  int8Array[position] = value;
 
-  typedArray[position] = value;
-
-  return typedArray;
+  if (position > int8Array.length) throw new Error('Position outside range');
+  return new DataView(dv);
 }
