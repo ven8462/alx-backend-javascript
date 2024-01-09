0244514678b00bff7afab3c44e9a6fb555f21bff
lavender <lavenderanyango9@gmail.com>
2024-01-09 23:58:30 +0300

    lint

--- 0x03-ES6_data_manipulation/5-typed_array.js
+++ 0x03-ES6_data_manipulation/5-typed_array.js
@@ -1,9 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  const dv = new ArrayBuffer(length);
-  const int8Array = new Int8Array(dv);
-
-  int8Array[position] = value;
-
-  if (position > int8Array.length) throw new Error('Position outside range');
-  return new DataView(dv);
+  if (position >= length) throw Error('Position outside range');
+  const buffer = new ArrayBuffer(length);
+  const newView = new DataView(buffer, 0, length);
+  const arr = new Int8Array(buffer);
+  arr[position] = value;
+  return newView;
 }
