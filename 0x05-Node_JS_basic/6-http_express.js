b344e2cce441bbf1a7b92d8bac8fd64881efe502
ven8462 <lavenderanyango9@gmail.com>
2024-02-27 20:53:40 +0000

    http express server

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,10 +1,11 @@
 const app = require('express')();
+
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
 });
-
 app.listen(port);
 
 module.exports = app;
+
