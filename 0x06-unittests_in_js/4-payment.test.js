f5187dcaac0e3706f1ff7e0027545877286e7bd3
Lavender Anyango <122857492+ven8462@users.noreply.github.com>
2024-03-01 20:36:53 +0300

    Update payment test

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,13 +3,15 @@ const Utils = require('./utils');
 const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./4-payment');
 
-describe('sendPaymentRequestToApi function', function () {
-  const spyUtil = sinon.spy(Utils, 'calculateNumber');
-
-  it('Utils function', function () {
+describe('Stubs', function () {
+  it('sendPaymentRequestToApi', function () {
+    const stubObj = sinon.stub(Utils, 'calculateNumber');
+    stubObj.returns(10);
+    const spyObj = sinon.spy(console, 'log');
     sendPaymentRequestToApi(100, 20);
-    expect(spyUtil.calledOnce).to.be.true;
-    expect(spyUtil.calledWith('SUM', 100, 20)).to.be.true;
-    spyUtil.restore();
+    expect(stubObj.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spyObj.calledOnceWithExactly('The total is: 10')).to.be.true;
+    stubObj.restore();
+    spyObj.restore();
   });
 });
